02da79723720f7e2bbb5783c0181de5437efd59b
sooshe <sooshe@sooshebot.com>
2020-07-17 20:15:36 -0500

    init commit ex1 --test failing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,16 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    # if the key is present, get() the key
+    for i in range(length):
+        results = cache.get(limit - weights[i])
+    
+        if results != None:
+            return i, results
+        
+        cache[weights[i]]
+
 
     return None
